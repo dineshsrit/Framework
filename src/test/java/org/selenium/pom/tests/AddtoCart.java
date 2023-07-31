7a6a2ee854f66da783a7aa3df51c79cb3acb264c
dineshsrit <dinesh.m477@gmail.com>
2023-07-31 19:42:28 +0530

    addtocart commit

---
+++ src/test/java/org/selenium/pom/tests/AddtoCart.java
@@ -0,0 +1,16 @@
+package org.selenium.pom.tests;
+
+import org.selenium.pom.base.BaseTest;
+import org.testng.annotations.Test;
+
+public class AddtoCart extends BaseTest {
+
+
+    @Test
+    public void addtoCartFromStorePage()
+    {
+
+        
+
+    }
+}
